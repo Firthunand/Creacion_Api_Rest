33bc902d594f459e3301ddff931dc329cd4b69a1
fernando de la barra <fdo.delabarra@gmail.com>
2023-09-22 13:07:58 -0300

    estandarizando codigos de retorno http de la api

--- src/main/java/med/voll/api/controller/MedicoController.java
+++ src/main/java/med/voll/api/controller/MedicoController.java
@@ -2,12 +2,14 @@ package med.voll.api.controller;
 
 import jakarta.transaction.Transactional;
 import jakarta.validation.Valid;
+import med.voll.api.direccion.DatosDireccion;
 import med.voll.api.medico.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.querydsl.QSort;
 import org.springframework.data.web.PageableDefault;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -29,17 +31,22 @@ public class MedicoController {
     }
     @PutMapping
     @Transactional
-    public void actualizarMedico(@RequestBody @Valid DatosActualizadosMedico datosActualizadosMedico){
+    public ResponseEntity actualizarMedico(@RequestBody @Valid DatosActualizadosMedico datosActualizadosMedico){
         Medico medico= medicoRepository.getReferenceById(datosActualizadosMedico.id());
         medico.actualizarDatos(datosActualizadosMedico);
+        return ResponseEntity.ok(new DatosRespuestaMedico(medico.getId(), medico.getNombre(), medico.getEmail(),
+                                medico.getTelefono(), medico.getEspecialidad().toString(),
+                                new DatosDireccion(medico.getDireccion().getCalle(), medico.getDireccion().getDistrito(),
+                                medico.getDireccion().getCiudad(), medico.getDireccion().getNumero(), medico.getDireccion().getComplemento())));
 
     }
     @DeleteMapping("/{id}")
     @Transactional
     //CON ESTE SE PODRIA DECIR QUE ES UN DELETE LOGICO AL DESACTIVAR MEDICO NO LO TRAERA CON LA LISTA
-    public void eliminarMedico(@PathVariable Long id){
+    public ResponseEntity eliminarMedico(@PathVariable Long id){
         Medico medico= medicoRepository.getReferenceById(id);
         medico.desactivarMedico();
+        return ResponseEntity.noContent().build();
 
     }
     //CON ESTE DELETE ELIMINAMOS DIRECTO DE LA BASE DE DATOS
